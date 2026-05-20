ef3ed9e3eb8a68a279f3f625d13d648c321df301
wolfye <wolfye@wolfye.wolfye>
2026-05-20 18:32:47 +0300

    Added compile.sh for calculate_scores and created the file. CALCULATE_SCORES START

---
+++ calculate_scores.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include "utils/inputValidation.h"
+#include "utils/fileFunctions.h"
+#include "utils/commands.h"
+#include "utils/report.h"
+
+
+
+int main(int argc, char *argv[]){
+
+    
+
+    return 0;
+}
---
+++ compileCalculateScores.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc -Wall -o calculate_scores calculate_scores.c
