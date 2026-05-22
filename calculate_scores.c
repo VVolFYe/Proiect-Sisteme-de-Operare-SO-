ed90803447a6a1d6faa78514125a44eb3a33ce6a
wolfye <wolfye@wolfye.wolfye>
2026-05-22 10:47:53 +0300

    Just created the calculate_scores.c program so it calculates and displays the scores for the district received in argument. Now i only have to connect the output with city hub

--- calculate_scores.c
+++ calculate_scores.c
@@ -1,42 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include "utils/inputValidation.h"
-#include "utils/fileFunctions.h"
-#include "utils/commands.h"
 #include "utils/report.h"
 
-void scorer(char *districtName){
-    printf("scorer function to be implemented [%s].\n", districtName);
-    // char command[256];
-    // snprintf(command, sizeof(command), "./scorer.sh %s", districtName);
-    execlp("./scorer.sh", "./scorer.sh", districtName, NULL);
-}
+#define MAX_INSPECTORS 100
 
-void calculate_scores(int argc, char *args[]){
-    
-    
-    for(int i = 1; i < argc; i++){
-        int pipefd[2];
-        if (pipe(pipefd) == -1) {
-            perror("pipe");
-            exit(-1);
+typedef struct {
+    char inspector[USER_LEN];
+    int score;
+} InspectorScore;
+
+int findInspector(InspectorScore scores[], int count, const char *name) {
+    for (int i = 0; i < count; i++) {
+        if (strcmp(scores[i].inspector, name) == 0) {
+            return i;
         }
-
-        close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[1]);
-
-        scorer(args[i]);
     }
+
+    return -1;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <district>\n", argv[0]);
+        return 1;
+    }
 
-    calculate_scores(argc, argv);
+    char *district = argv[1];
+
+    char reportsPath[256];
+    snprintf(reportsPath, sizeof(reportsPath), "./%s/reports.dat", district);
+
+    int fd = open(reportsPath, O_RDONLY);
+
+    if (fd == -1) {
+        printf("District [%s]: could not open reports.dat\n", district);
+        return 1;
+    }
+
+    InspectorScore scores[MAX_INSPECTORS];
+    int count = 0;
+
+    Report r;
+
+    while (read(fd, &r, sizeof(Report)) == sizeof(Report)) {
+        int pos = findInspector(scores, count, r.inspector);
+
+        if (pos == -1) {
+            if (count >= MAX_INSPECTORS) {
+                printf("District [%s]: too many inspectors\n", district);
+                close(fd);
+                return 1;
+            }
+
+            strncpy(scores[count].inspector, r.inspector, USER_LEN - 1);
+            scores[count].inspector[USER_LEN - 1] = '\0';
+            scores[count].score = r.severity;
+            count++;
+        } else {
+            scores[pos].score += r.severity;
+        }
+    }
+
+    close(fd);
+
+    printf("District: %s\n", district);
+
+    if (count == 0) {
+        printf("no reports found\n");
+        return 0;
+    }
+
+    for (int i = 0; i < count; i++) {
+        printf("inspector=%s;score=%d\n", scores[i].inspector, scores[i].score);
+    }
+
 
     return 0;
 }
